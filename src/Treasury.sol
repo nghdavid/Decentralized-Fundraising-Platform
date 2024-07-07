8dc19a64cdc6c17b8f5e3721c1bad5ba8fbedb99
Kuan-Hao Chen <35591407+nghdavid@users.noreply.github.com>
2024-07-07 22:53:35 +0800

    feat: Finish first version

---
+++ src/Treasury.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (finance/VestingWallet.sol)
+pragma solidity ^0.8.20;  
+import "@openzeppelin/contracts/finance/VestingWallet.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {IERC20Mint} from "./interfaces/IERC20.sol";
+contract Treasury is VestingWallet, AccessControl {
+    bytes32 public constant DAO_ROLE = keccak256("DAO_ROLE");
+    address public token;
+    address public daoToken;
+    uint64 public startTimestamp;
+    uint256 private _totalShares;
+    mapping(address => uint256) private _shares;
+    address[] private _payees;
+
+    event PayeeAdded(address account, uint256 shares);
+
+    constructor(
+        address beneficiary,
+        address dao,
+        uint64 _startTimestamp,
+        uint64 _durationSeconds,
+        address _token,
+        address _daoToken
+    ) VestingWallet(beneficiary, _startTimestamp, _durationSeconds) {
+        _grantRole(DAO_ROLE, dao);
+        token = _token;
+        daoToken = _daoToken;
+        startTimestamp = _startTimestamp;
+    }
+
+    function withdrawToInvestor() public onlyRole(DAO_ROLE) {}
+
+    function receiveFromInvestor(uint256 amount) public {
+        require(block.timestamp < startTimestamp, "Funding has ended");
+        IERC20Mint fundedToken = IERC20Mint(token);
+        require(
+            fundedToken.allowance(msg.sender, address(this)) >= amount,
+            "Insufficient allowance"
+        );
+        fundedToken.transferFrom(msg.sender, address(this), amount);
+        _addPayee(msg.sender, amount);
+        IERC20Mint voteToken = IERC20Mint(daoToken);
+        voteToken.mint(msg.sender, amount);
+    }
+
+    function _addPayee(address account, uint256 shares_) private {
+        require(account != address(0));
+        if (_shares[account] == 0) {
+            _payees.push(account);
+            _shares[account] = shares_;
+        } else {
+            _shares[account] += shares_;
+        }
+        _totalShares += shares_;
+        emit PayeeAdded(account, shares_);
+    }
+}
---
+++ src/VoteToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract VoteToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
+    constructor(address initialOwner) ERC20("VoteToken", "VTK") ERC20Permit("VoteToken") Ownable(initialOwner) {}
+
+    // The functions below are overrides required by Solidity.
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+
+    function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
