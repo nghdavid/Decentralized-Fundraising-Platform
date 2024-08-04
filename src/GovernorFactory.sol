e455e9e9786815d6114fc412dbeff2f6902aa6bb
Kuan-Hao Chen <35591407+nghdavid@users.noreply.github.com>
2024-08-04 12:23:25 +0800

    chore: Remove useless part

--- src/GovernorFactory.sol
+++ src/GovernorFactory.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./TimeLock.sol";
-import "./Governor.sol";
-import {Test, console} from "forge-std/Test.sol";
-import "@openzeppelin/contracts/utils/Create2.sol";
+import {MyGovernor} from "./Governor.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 
@@ -12,26 +9,19 @@ contract GovernorFactory {
     function createDao(
         string memory _daoName,
         address voteToken,
-        address timelock
+        address timelockAddress
     ) public returns (address daoAddress) {
-        bytes32 salt = keccak256(
-            abi.encodePacked(timelock, voteToken)
+        bytes32 salt = keccak256(abi.encodePacked(timelockAddress, voteToken));
+        TimelockController timelock = TimelockController(
+            payable(timelockAddress)
         );
-        TimelockController timelock = TimelockController(payable(timelock));
-        MyGovernor governor = new MyGovernor{salt: salt}(IVotes(voteToken), timelock, _daoName);
+        MyGovernor governor = new MyGovernor{salt: salt}(
+            IVotes(voteToken),
+            timelock,
+            _daoName
+        );
         timelock.grantRole(timelock.PROPOSER_ROLE(), address(governor));
         timelock.grantRole(timelock.EXECUTOR_ROLE(), address(governor));
         daoAddress = address(governor);
-    }
-
-    function calculateDaoAddr(
-        address timelock,
-        address voteToken,
-        string memory daoName
-    ) public view returns (address predictedAddress) {
-        bytes32 salt = keccak256(abi.encodePacked(timelock, voteToken));
-        bytes memory constructorArgs = abi.encode(voteToken, timelock, daoName);
-        bytes memory bytecode = abi.encodePacked(type(MyGovernor).creationCode, constructorArgs);
-        predictedAddress = Create2.computeAddress(salt, keccak256(bytecode));
     }
 }
